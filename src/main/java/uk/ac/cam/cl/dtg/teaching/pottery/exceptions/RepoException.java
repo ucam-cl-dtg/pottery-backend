2141e38f98234d647d3bf170d4e601f77d403806
Andrew Rice <acr31@cam.ac.uk>
2015-05-01 12:08:53 +0100

    Exception for problems with repositories

---
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/exceptions/RepoException.java
@@ -0,0 +1,30 @@
+package uk.ac.cam.cl.dtg.teaching.pottery.exceptions;
+
+public class RepoException extends Exception {
+
+	public RepoException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public RepoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RepoException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RepoException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RepoException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
