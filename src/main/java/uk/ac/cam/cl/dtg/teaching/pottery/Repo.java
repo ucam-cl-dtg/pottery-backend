09a0a418dd2be9a3e2558a6f22083110adf103be
Andrew Rice <acr31@cam.ac.uk>
2015-07-17 10:44:45 +0100

    added methods to the repo object so it can be stored in the database

--- src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/Repo.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/Repo.java
@@ -1,10 +1,31 @@
 package uk.ac.cam.cl.dtg.teaching.pottery;
 
+import com.mongodb.QueryBuilder;
+
 public class Repo {
 
+	private String _id;
 	private String repoId;
 	private String taskId;
 	
+	public Repo(String repoId, String taskId) {
+		super();
+		this.repoId = repoId;
+		this.taskId = taskId;
+	}
+	
+	
+	public Repo() {
+		super();
+	}
+
+
+	public String get_id() {
+		return _id;
+	}
+	public void set_id(String _id) {
+		this._id = _id;
+	}
 	public String getRepoId() {
 		return repoId;
 	}
@@ -18,6 +39,11 @@ public class Repo {
 		this.taskId = taskId;
 	}
 	
+	public static Repo getByRepoId(String repoId, Database database) {
+		return database.getCollection(Repo.class).findOne(QueryBuilder.start("repoId").is(repoId).get());
+	}
 	
-	
+	public void insert(Database database) {
+		database.getCollection(Repo.class).insert(this);
+	}
 }
