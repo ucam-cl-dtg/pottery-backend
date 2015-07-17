cac13892f58cf4c5d386056de47988a7f1539489
Andrew Rice <acr31@cam.ac.uk>
2015-07-17 10:44:23 +0100

    helper methods for wrapping a collection

--- src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/Database.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/Database.java
@@ -2,11 +2,13 @@ package uk.ac.cam.cl.dtg.teaching.pottery;
 
 import java.net.UnknownHostException;
 
+import org.mongojack.JacksonDBCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.inject.Singleton;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
 @Singleton
@@ -15,7 +17,7 @@ public class Database {
 	private DB db;
 	
 	private static final Logger log = LoggerFactory.getLogger(Database.class);
-	
+
 	public Database() {
 		try {
 			db = new MongoClient("localhost").getDB("ptest");
@@ -25,5 +27,16 @@ public class Database {
 	}
 	
 	public DB getDb() { return db; }
+
+	public <T> JacksonDBCollection<T, String> getCollection(Class<T> clazz, String name) {
+		DBCollection collection = db.getCollection(name);
+		return JacksonDBCollection.wrap(collection, clazz, String.class);
+	}
+
+	public <T> JacksonDBCollection<T, String> getCollection(Class<T> clazz) {
+		DBCollection collection = db.getCollection(clazz.getName());
+		return JacksonDBCollection.wrap(collection, clazz, String.class);
+	}
+
 	
 }
