486f6e67605353fe0a54608233304f2976db315e
Andrew Rice <acr31@cam.ac.uk>
2016-04-07 18:12:30 +0100

    Call a linux binary to get the UID of the current user
    I need this so I can call sudo to make sure that the files written inside the container are editable by the webserver process

--- src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/app/Config.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/app/Config.java
@@ -1,11 +1,63 @@
 package uk.ac.cam.cl.dtg.teaching.pottery.app;
 
 import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Config {
 
+	public static final Logger LOG = LoggerFactory.getLogger(Config.class);
+	
 	private static final File PREFIX= new File("/opt/pottery");
 	
+	private String userName;
+
+	private int uid;
+	
+	public Config() {
+		this.userName = System.getProperty("user.name");
+		this.uid = getUID(userName);
+	}
+	
+	private static int getUID(String userName) {
+	    try {
+			Process child = Runtime.getRuntime().exec("/usr/bin/id -u "+userName);
+			try {
+				return Integer.parseInt(IOUtils.readLines(child.getInputStream()).get(0));
+			}
+			finally {
+				child.destroyForcibly();
+			}
+		} catch (NumberFormatException e) {
+			LOG.error("Failed to parse UID",e);
+			return 0;
+		} catch (IOException e) {
+			LOG.error("Failed to run id to find UID");
+			return 0;
+		}
+	}
+	
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
 	public File getRepoRoot() {
 		return new File(PREFIX,"repos");
 	}
@@ -22,17 +74,16 @@ public class Config {
 		return "HEAD";
 	}
 	
-	
 	public String getWebtagPrefix() {
 		return "online-";
 	}
 
-	public File getRegisteredTaskRoot() {
-		return new File(PREFIX,"working-tasks");
-	}
-	
 	public File getTaskRepoRoot() {
 		return new File(PREFIX,"tasks-bare");
+	}
+	
+	public File getRegisteredTaskRoot() {
+		return new File(PREFIX,"working-tasks");
 	}
 	
 }
