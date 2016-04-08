75fd5f86c46a7f542764a6bedf12b13e3ecb7db2
Andrew Rice <acr31@cam.ac.uk>
2016-04-08 10:25:18 +0100

    Scan for registered tasks on startup and clone any new ones
    Keep track of used UUIDs so we can reserve new ones

--- src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/managers/TaskManager.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/managers/TaskManager.java
@@ -4,8 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ResetCommand.ResetType;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Ref;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,38 +19,100 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+import uk.ac.cam.cl.dtg.teaching.pottery.FileUtil;
+import uk.ac.cam.cl.dtg.teaching.pottery.UUIDGenerator;
 import uk.ac.cam.cl.dtg.teaching.pottery.app.Config;
+import uk.ac.cam.cl.dtg.teaching.pottery.app.RegistrationTag;
 import uk.ac.cam.cl.dtg.teaching.pottery.dto.Task;
+import uk.ac.cam.cl.dtg.teaching.pottery.exceptions.InvalidTagFormatException;
 
 @Singleton
 public class TaskManager {
 
 	private File checkoutLocation;
+	
+	/**
+	 * A map of taskId (uuid) to Task objects
+	 */
+	private Map<String,Task> tasks;
 
-	private Map<String,Task> tasks = new HashMap<String,Task>();
-
+	/**
+	 * A map of taskId (uiid) to the repository it came from
+	 */
+	private Map<String,RegistrationTag> taskRegistrations;
+	
+	private UUIDGenerator uuidGenerator;
+	
 	private static final Logger log = LoggerFactory.getLogger(TaskManager.class);
 	
+	
 	@Inject
-	public TaskManager(Config config) {
-		checkoutLocation = config.getRegisteredTaskRoot();
+	public TaskManager(Config config) throws IOException, GitAPIException, InvalidTagFormatException {
+		File taskReposDir = config.getTaskRepoRoot();
 
-		ObjectMapper o = new ObjectMapper();
-		for(File f : checkoutLocation.listFiles()) {
-			if (f.getName().startsWith(".")) continue;			
-			File taskSpecFile = new File(f,"task.json");
-			if (taskSpecFile.exists()) {
-				try {
-					Task t = o.readValue(taskSpecFile,Task.class);
-					tasks.put(t.getTaskId(), t);
-				} catch (IOException e) {
-					log.warn("Failed to load task "+taskSpecFile,e);
-				}
+		this.checkoutLocation = config.getRegisteredTaskRoot();
+		this.taskRegistrations = scanForTasks(taskReposDir);
+		this.tasks = new HashMap<>();
+		this.uuidGenerator = new UUIDGenerator();
+		uuidGenerator.reserveAll(taskRegistrations.keySet());
+
+		for (Map.Entry<String,RegistrationTag> entry : taskRegistrations.entrySet()) {
+			String uuid = entry.getKey();
+			RegistrationTag r = entry.getValue();
+			File dir = new File(checkoutLocation,uuid);
+			if (!dir.exists()) {
+				cloneTask(r);
 			}
-			else {
-				log.warn("Failed to find task.json file for task in {}",taskSpecFile);
+			Task t = loadTask(uuid,r.isActive(), new File(dir,"task.json"));
+			if (t!=null) {
+				tasks.put(t.getTaskId(), t);
 			}
+		}		
+	}
+	
+	/**
+	 * Take a clone of the task repository at the version given and use it as a new task
+	 * 
+	 * @param registrationTag
+	 * @return a task object containing information about the task
+	 * @throws InvalidTagFormatException
+	 * @throws GitAPIException
+	 * @throws IOException
+	 */
+	public Task cloneTask(RegistrationTag registrationTag) throws InvalidTagFormatException, GitAPIException, IOException {
+		
+		// TODO: we also need to compile the source code for the test...
+		String uuid = registrationTag.getRegisteredTaskUUID();
+		File taskDir = new File(checkoutLocation,uuid);
+		Task t = null;
+		try {
+			try (Git g = Git.cloneRepository().setURI(registrationTag.getRepository().getPath()).setDirectory(taskDir).call()) {
+				g.reset().setMode(ResetType.HARD).setRef(registrationTag.toTagName()).call();
+			}
+			t = loadTask(uuid,registrationTag.isActive(),new File(taskDir,"task.json"));
 		}
+		finally {
+			if (t == null)
+				FileUtil.deleteRecursive(taskDir);
+		}
+		if (t != null) 
+			tasks.put(t.getTaskId(), t);
+		return t;		
+	}
+	
+	/**
+	 * Allocate a new UUID for a task (make sure its unique)
+	 * @return the new UUID
+	 */
+	public String reserveNewTaskUuid() {
+		return uuidGenerator.generate();
+	}
+	
+	public RegistrationTag getRegistration(String taskId) {
+		return taskRegistrations.get(taskId);
+	}
+	
+
 	/**
 	 * Build a map for all registered tasks by scanning for tags in the task repositories
 	 * 
