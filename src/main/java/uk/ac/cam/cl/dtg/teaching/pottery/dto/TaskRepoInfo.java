e2f2eff4681a87ca8e2641ba16e11004140fcea2
Andrew Rice <acr31@cam.ac.uk>
2016-04-08 11:36:09 +0100

    Added list of registered tasks to taskrepoinfo

--- src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/dto/TaskRepoInfo.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/dto/TaskRepoInfo.java
@@ -1,21 +1,36 @@
 package uk.ac.cam.cl.dtg.teaching.pottery.dto;
 
-public class TaskRepoInfo {
+import java.util.LinkedList;
+import java.util.List;
 
-	private String name;
+public class TaskRepoInfo implements Comparable<TaskRepoInfo> {
 
-	public TaskRepoInfo(String name) {
-		this.name = name;
+	private String taskRepoId;
+	private List<Task> registeredTasks = new LinkedList<>();
+	
+	public TaskRepoInfo(String taskRepoId) {
+		super();
+		this.taskRepoId = taskRepoId;
 	}
-
-	public String getName() {
-		return name;
+	public String getTaskRepoId() {
+		return taskRepoId;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	public void setTaskRepoId(String taskRepoId) {
+		this.taskRepoId = taskRepoId;
 	}
-
+	public List<Task> getRegisteredTasks() {
+		return registeredTasks;
+	}
+	public void setRegisteredTasks(List<Task> registeredTasks) {
+		this.registeredTasks = registeredTasks;
+	}
+	
+	@Override
+	public int compareTo(TaskRepoInfo o) {
+		return taskRepoId.compareTo(o.taskRepoId);
+	}
+	
+	
 	
 	
 }
