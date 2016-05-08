861c3d7b559cf90de173945a08e4cbe1c77528a3
Andrew Rice <acr31@cam.ac.uk>
2016-05-08 12:07:39 +0100

    Added 'acceptable' flag to validation steps

--- src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/dto/ValidationStep.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/dto/ValidationStep.java
@@ -4,16 +4,26 @@ public class ValidationStep {
 	
 	private String criterion;
 	private String result;
+	private boolean acceptable;
 	private String[] references;
 	
-	public ValidationStep(String criterion, String result, String... references) {
+	public ValidationStep(String criterion, String result, boolean acceptable, String... references) {
 		this.criterion = criterion;
 		this.result = result;
+		this.acceptable = acceptable;
 		this.references = references;
 	}
 	
 	public ValidationStep() {
 		
+	}	
+	
+	public boolean isAcceptable() {
+		return acceptable;
+	}
+
+	public void setAcceptable(boolean acceptable) {
+		this.acceptable = acceptable;
 	}
 
 	public String getCriterion() {
