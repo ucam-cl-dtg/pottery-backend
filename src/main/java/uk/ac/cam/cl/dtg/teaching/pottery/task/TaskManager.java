d49df461cb21f066b450dd430485357d236acfa4
Andrew Rice <acr31@cam.ac.uk>
2016-05-30 16:33:20 +0100

    Simplified TaskManager by removing static caches of retired and registered tasks

--- src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/task/TaskManager.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/task/TaskManager.java
@@ -22,7 +22,7 @@ import uk.ac.cam.cl.dtg.teaching.pottery.containers.ContainerManager;
 import uk.ac.cam.cl.dtg.teaching.pottery.dto.TaskInfo;
 import uk.ac.cam.cl.dtg.teaching.pottery.exceptions.TaskCloneException;
 import uk.ac.cam.cl.dtg.teaching.pottery.exceptions.TaskException;
-import uk.ac.cam.cl.dtg.teaching.pottery.exceptions.TaskRegistrationException;
+import uk.ac.cam.cl.dtg.teaching.pottery.exceptions.TaskNotFoundException;
 
 @Singleton
 public class TaskManager {
@@ -30,22 +30,13 @@ public class TaskManager {
 	public static final Logger LOG = LoggerFactory.getLogger(TaskManager.class);
 			
 	private Map<String,Task> definedTasks;
-	private Map<String,Task> retiredTasks;
-	private Map<String,Task> registeredTasks;
 
 	private TaskFactory taskFactory;
-	private ContainerManager containerManager;
-	private Database database;
 	
 	@Inject
-	public TaskManager(ContainerManager containerManager, Database database, TaskFactory taskFactory) throws GitAPIException, IOException, SQLException, TaskException {
-		this.containerManager = containerManager;
-		
+	public TaskManager(ContainerManager containerManager, TaskFactory taskFactory, Database database) throws GitAPIException, IOException, SQLException, TaskException {
 		this.taskFactory = taskFactory;
-		this.database = database;
 		this.definedTasks = new HashMap<>();
-		this.registeredTasks = new HashMap<>();
-		this.retiredTasks = new HashMap<>();
 		
 		List<String> taskIds;
 		try(TransactionQueryRunner q = database.getQueryRunner()) {
@@ -56,12 +47,6 @@ public class TaskManager {
 			try {
 				Task t = taskFactory.getInstance(taskId);
 				definedTasks.put(taskId, t);
-				if (t.isRetired()) {
-					retiredTasks.put(taskId,t);
-				}
-				if (t.getRegisteredClone() != null) {
-					registeredTasks.put(taskId, t);
-				}
 			}
 			catch (TaskException e) {
 				LOG.warn("Ignoring task "+taskId,e);
@@ -75,52 +60,40 @@ public class TaskManager {
 		return newTask.getTestingClone().getInfo();
 	}
 	
-	/**
-	 * Register this particular version of the task for others to use.
-	 * 
-	 * This works by cloning (if necessary) the task bare repo and then resetting it to the desired sha1. Re-registering a task just changes the version that is checked out.
-	 *  
-	 *  @param taskId is the uuid of the task to register
-	 *  @param sha1 is the version of the commit to register
-	 * @throws TaskRegistrationException 
-	 * @throws IOException 
-	 * @throws TaskCloneException 
-	 * @throws TaskException 
-	 * @throws SQLException 
-	 */
-	public TaskInfo registerTask(String taskId, String sha1) throws TaskRegistrationException, TaskException, TaskCloneException, IOException, SQLException {
-		Task task = definedTasks.get(taskId);
-		task.registerTask(sha1, containerManager,database);
-		registeredTasks.put(taskId, task);
-		return task.getRegisteredClone().getInfo();
-	}
-	
 	public Collection<TaskInfo> getTestingTasks() {
 		return definedTasks.values().stream().map(t -> t.getTestingClone().getInfo()).collect(Collectors.toList());
 	}
 	
 	public Collection<TaskInfo> getRegisteredTasks() {
-		return registeredTasks.values().stream().map(t -> t.getRegisteredClone().getInfo()).collect(Collectors.toList());
+		return definedTasks.values().stream()
+				.filter(t -> t.getRegisteredClone() != null)
+				.map(t -> t.getRegisteredClone().getInfo())
+				.collect(Collectors.toList());
 	}
 
 	public Collection<TaskInfo> getRetiredTasks() {
-		return retiredTasks.values().stream().map(t -> t.getTestingClone().getInfo()).collect(Collectors.toList());
+		return definedTasks.values().stream()
+				.filter(t -> t.isRetired())
+				.map(t -> t.getTestingClone().getInfo())
+				.collect(Collectors.toList());
 	}
 
 	public TaskInfo getTestingTask(String taskId) {
 		return definedTasks.get(taskId).getTestingClone().getInfo();
 	}
 
-	public TaskInfo getRegisteredTaskInfo(String taskId) {
-		return registeredTasks.get(taskId).getRegisteredClone().getInfo();
+	public TaskInfo getRegisteredTaskInfo(String taskId) throws TaskNotFoundException {
+		TaskClone c = definedTasks.get(taskId).getRegisteredClone();
+		if (c == null) throw new TaskNotFoundException("Failed to find a registered task with ID "+taskId);
+		return c.getInfo();
 	}
 
 	public Task getTask(String taskId) {
 		return definedTasks.get(taskId);
 	}
 	
-	public void updateTesting(String taskID) throws TaskCloneException {
-		definedTasks.get(taskID).getTestingClone().update(Constants.HEAD);
+	public void updateTesting(String taskID) throws TaskCloneException, IOException {
+		definedTasks.get(taskID).getTestingClone().update(Constants.HEAD,true);
 	}
 
 }
