550a3cc3061c96bdbd7f1be685764160f5a50990
Andrew Rice <acr31@cam.ac.uk>
2016-06-16 15:21:23 +0100

    removed unused import

--- src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/controllers/TasksController.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/controllers/TasksController.java
@@ -12,7 +12,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
