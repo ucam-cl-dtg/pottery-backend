ef48904c7819549789670b481673c4197327a5c0
Andrew Rice <acr31@cam.ac.uk>
2016-06-17 07:26:57 +0100

    used readwritelock to tidy up concurrency on TaskClone

--- src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/task/TaskClone.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/pottery/task/TaskClone.java
@@ -11,6 +11,7 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.apache.commons.io.IOUtils;
 import org.eclipse.jgit.api.Git;
@@ -32,12 +33,31 @@ public class TaskClone {
 	
 	private File repo;
 	
-	private TaskInfo info;
+	private volatile TaskInfo info;
 	
 	private final File upstreamRepo;
 	
+	/**
+	 * Lock controlling access to the filesystem permitting multiple concurrent readers and exclusive writers
+	 * 
+	 * If you are using the git database only then you just need a read lock
+	 * 
+	 * If you are doing something that will destroy the git database (clone, move) then you need the write lock
+	 * 
+	 * If you are doing something that affects the working directory then you need the read lock as the fs lock below
+	 */
+	private final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+	
+	/**
+	 * Lock providing mutual exclusion on working directory operations. Acquire this last.
+	 * You need to hold this even if you are only reading files in the working directory
+	 */
+	private final Object fileLock = new Object();
+	
 	/** 
-	 * Do not create directly. Access through the members of the Task object
+	 * Do not create directly. Access through the members of the Task object. This is because
+	 * we want to ensure that there is only one instance of this object for each repo 
+	 * directory so that we can control concurrent access
 	 * @throws GitAPIException 
 	 * @throws IOException 
 	 */
@@ -45,84 +65,131 @@ public class TaskClone {
 		this.repo = repo;
 		this.upstreamRepo = upstreamRepo;
 		update(tag,false);
-		this.info = TaskInfo.load(repo);
+		// this.info is filled in by the update method
 	}
 
-	public synchronized String getHeadSHA() throws TaskCloneException {
-		try (Git g = Git.open(repo)) {
-			Ref r = g.getRepository().findRef(Constants.HEAD);
-			return r.getObjectId().getName();
-		} catch (IOException e) {
-			throw new TaskCloneException("Failed to read Git repository for "+repo,e);	
+	public String getHeadSHA() throws TaskCloneException {
+		readWriteLock.readLock().lock();;
+		try {
+			try (Git g = Git.open(repo)) {
+				Ref r = g.getRepository().findRef(Constants.HEAD);
+				return r.getObjectId().getName();
+			} catch (IOException e) {
+				throw new TaskCloneException("Failed to read Git repository for "+repo,e);	
+			}
+		}
+		finally {
+			readWriteLock.readLock().unlock();
 		}
 	}
 	
-	public synchronized void update(String tag, boolean fromScratch) throws TaskCloneException, IOException {
+	public void update(String tag, boolean fromScratch) throws TaskCloneException, IOException {
 		LOG.debug("Updating {} to {}",repo,tag);
-		if (fromScratch) FileUtil.deleteRecursive(repo);
 		
-		if (!repo.exists()) {
-			try (Git g = Git.cloneRepository().setURI(upstreamRepo.getPath()).setDirectory(repo).call()) {
-				g.reset().setMode(ResetType.HARD).setRef(tag).call();
-			} catch (GitAPIException e) {
-				throw new TaskCloneException("Failed to create clone of "+upstreamRepo+" and reset to "+tag,e);
-			}			
+		if (fromScratch || !repo.exists()) {
+			readWriteLock.writeLock().lock();
+			try {
+				FileUtil.deleteRecursive(repo);
+				try (Git g = Git.cloneRepository().setURI(upstreamRepo.getPath()).setDirectory(repo).call()) {
+					g.reset().setMode(ResetType.HARD).setRef(tag).call();
+					info = TaskInfo.load(repo);
+					return;
+				} catch (GitAPIException e) {
+					throw new TaskCloneException("Failed to create clone of "+upstreamRepo+" and reset to "+tag,e);
+				}
+			}
+			finally {
+				readWriteLock.writeLock().unlock();
+			}
 		}
 		else {
-			try (Git g = Git.open(repo)) {
-				PullResult p = g.pull().setRemote("origin").call();
-				if (p.isSuccessful()) {
-					g.reset().setMode(ResetType.HARD).setRef(tag).call();
-				}
-			} catch (IOException | GitAPIException e) {
-				// if we failed to do the lightweight update then try again with a full delete and clone
-				if (!fromScratch) update(tag,true);
-				throw new TaskCloneException("Failed to update "+repo+" to tag "+tag,e);
-			}
-		}
-		info = TaskInfo.load(repo);
-	}
-	
-	public synchronized List<String> copySkeleton(File destination) throws IOException {
-		File sourceLocation = getSkeletonRoot();
-		if (!sourceLocation.exists()) { return new LinkedList<>(); }
-		List<String> copiedFiles = new LinkedList<>();
-		Files.walkFileTree(sourceLocation.toPath(), new SimpleFileVisitor<Path>() {
-			@Override
-			public FileVisitResult visitFile(Path file,
-					BasicFileAttributes attrs) throws IOException {
-				
-				File originalFile = file.toFile();
-				Path localLocation = sourceLocation.toPath().relativize(file);
-				copiedFiles.add(localLocation.toString());
-				File newLocation = destination.toPath().resolve(localLocation).toFile();
-				File newDir = newLocation.getParentFile();
-
-				if (newDir.exists()) {
-					newDir.mkdirs();
-				}
-				
-				try(FileOutputStream fos = new FileOutputStream(newLocation)) {
-					try(FileInputStream fis = new FileInputStream(originalFile)) {
-						IOUtils.copy(fis, fos);
+			Exception caught = null;
+			readWriteLock.readLock().lock();
+			try {
+				synchronized (fileLock) {
+					try (Git g = Git.open(repo)) {
+						PullResult p = g.pull().setRemote("origin").call();
+						if (p.isSuccessful()) {
+							g.reset().setMode(ResetType.HARD).setRef(tag).call();
+						}
+					} catch (IOException | GitAPIException e) {
+						// if we failed to do the lightweight update then try again with a full delete and clone
+						// but don't call it here because we already hold the read lock so we'll deadlock
+						caught = e;
 					}
 				}
-				return FileVisitResult.CONTINUE;
+				info = TaskInfo.load(repo);
 			}
-		});
-		return copiedFiles;
+			finally {
+				readWriteLock.readLock().unlock();
+			}
+			
+			if (caught != null) {
+				try {
+					update(tag,true);
+				}
+				catch (IOException | TaskCloneException e) {
+					e.addSuppressed(caught);
+					throw e;
+				}
+			}
+		}
+	}
+	
+	public List<String> copySkeleton(File destination) throws IOException {
+		readWriteLock.readLock().lock();
+		try {
+			synchronized (fileLock) {
+				File sourceLocation = getSkeletonRoot();
+				if (!sourceLocation.exists()) { return new LinkedList<>(); }
+				List<String> copiedFiles = new LinkedList<>();
+				Files.walkFileTree(sourceLocation.toPath(), new SimpleFileVisitor<Path>() {
+					@Override
+					public FileVisitResult visitFile(Path file,
+							BasicFileAttributes attrs) throws IOException {
+
+						File originalFile = file.toFile();
+						Path localLocation = sourceLocation.toPath().relativize(file);
+						copiedFiles.add(localLocation.toString());
+						File newLocation = destination.toPath().resolve(localLocation).toFile();
+						File newDir = newLocation.getParentFile();
+
+						if (newDir.exists()) {
+							newDir.mkdirs();
+						}
+
+						try(FileOutputStream fos = new FileOutputStream(newLocation)) {
+							try(FileInputStream fis = new FileInputStream(originalFile)) {
+								IOUtils.copy(fis, fos);
+							}
+						}
+						return FileVisitResult.CONTINUE;
+					}
+				});
+				return copiedFiles;
+			}
+		}
+		finally {
+			readWriteLock.readLock().unlock();
+		}
 	}
 	
 	public TaskInfo getInfo() {
 		return info;
 	}
 
-	public synchronized void move(File taskRegisteredRoot) throws IOException {
-		FileUtil.deleteRecursive(taskRegisteredRoot);
-		if (!repo.renameTo(taskRegisteredRoot)) {
-			throw new IOException("Failed to move clone from "+repo+" to "+taskRegisteredRoot);
+	public void move(File taskRegisteredRoot) throws IOException {
+		readWriteLock.writeLock().lock();
+		try {
+			FileUtil.deleteRecursive(taskRegisteredRoot);
+			if (!repo.renameTo(taskRegisteredRoot)) {
+				throw new IOException("Failed to move clone from "+repo+" to "+taskRegisteredRoot);
+			}
+			repo = taskRegisteredRoot;
 		}
-		repo = taskRegisteredRoot;
+		finally {
+			readWriteLock.writeLock().unlock();
+		}
 	}
 	
 	private File getSkeletonRoot() {
@@ -142,8 +209,14 @@ public class TaskClone {
 	}
 	
 	public void runInContext(RunnableWithinTask t) {
-		synchronized (repo) {
-			t.run(getCompileRoot(), getHarnessRoot(), getValidatorRoot());
+		readWriteLock.readLock().lock();
+		try {
+			synchronized (fileLock) {
+				t.run(getCompileRoot(), getHarnessRoot(), getValidatorRoot());
+			}
+		}
+		finally {
+			readWriteLock.readLock().unlock();
 		}
 	}
 	
